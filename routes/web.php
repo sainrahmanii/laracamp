34d329f94669f4a3ffc5fb8349220ac6dccccf4e
Sainrahmani12 <husainrahmani127@gmail.com>
2022-08-28 13:40:18 +0700

    slicing dashboard,login,checkouts,success_checkout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.index');
 });
+
+Route::get('/login', function() { return view('layouts.login'); })->name('login');
+Route::get('/checkouts', function() { return view('layouts.checkouts'); })->name('checkouts');
+
