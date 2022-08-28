2c4f215f9e99ad9dbc7e2daf0bfb2a2c5da00627
Sainrahmani12 <husainrahmani127@gmail.com>
2022-08-28 11:37:02 +0700

    create seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = [
+            [
+                'name'                  => 'admin',
+                'email'                 => 'admin@laracamp.com',
+                'email_verified_at'     => date('Y-m-d H:i:s', time()),
+                'password'              => bcrypt('password'),
+                'is_admin'              => true
+            ]
+        ];
+
+        User::insert($admin);
+    }
+}
