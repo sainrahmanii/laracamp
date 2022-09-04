a871cf21175f54a7ad163a396e5a8636322f92ec
Sainrahmani12 <husainrahmani127@gmail.com>
2022-09-04 15:17:46 +0700

    update checkout page

--- app/Http/Requests/User/Checkouts/Store.php
+++ app/Http/Requests/User/Checkouts/Store.php
@@ -26,12 +26,11 @@ class Store extends FormRequest
     {
         $expiredValidation = date('Y-m', time());
         return [
-            'name'      => 'required',
-            'email'     => 'required',
-            'occupation'=> 'required',
-            'card_number'=> 'required|numeric|digits_between:8,16',
-            'expired'=> 'required|date|date_format:Y-m|after_or_equal:'.$expiredValidation,
-            'cvc'=> 'required|numeric|digits:3',
+            'name'      => 'required|string',
+            'email'     => 'required|email|unique:users,email,'.Auth::id().',id',
+            'occupation'=> 'required|string',
+            'phone'     => 'required|string',
+            'address'   => 'required|string',
         ];
     }
 }
