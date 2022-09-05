3493dfe438788c7a2a0630f157696acc17fd24eb
Sainrahmani12 <husainrahmani127@gmail.com>
2022-09-05 13:47:00 +0700

    oauth google

--- config/services.php
+++ config/services.php
@@ -35,7 +35,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_CLIENT_REDIRECT'),
+        'redirect' => 'http://husainrahmani-laracamp.herokuapp.com/auth/google/callback',
     ],
 
 ];
